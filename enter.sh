3dad2469adc9d4605efb038335d9c481c2fe166a
Gijs Molenaar <gijs@pythonic.nl>
2014-03-06 12:12:59 +0200

    loop it

--- enter.sh
+++ enter.sh
@@ -2,16 +2,16 @@
 
 SUITE=precise
 
-sudo mount -o bind /proc ${SUITE}/proc
-sudo mount -o bind /sys ${SUITE}/sys
-sudo mount -o bind /dev ${SUITE}/dev
-sudo mount -o bind /run ${SUITE}/run
-sudo mount -o bind /home/gijs ${SUITE}/home/gijs
+MAP="/proc /sys /dev /run /home/gijs"
+
+
+for i in $MAP; do
+    sudo mount -o bind $i ${SUITE}$i
+done
 
 sudo chroot ${SUITE} /bin/bash
 
-sudo umount ${SUITE}/proc
-sudo umount ${SUITE}/sys
-sudo umount ${SUITE}/dev
-sudo umount ${SUITE}/run
-sudo umount ${SUITE}/build
+for i in $MAP; do
+    sudo umount  ${SUITE}$i
+done
+
